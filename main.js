2c43bed12949ba8237561056de7bbdc6c55f28f1
Rami Naser <rmnaser90@gmail.com>
2022-03-25 10:37:47 +0300

    adding fav city to localstorage feature

--- main.js
+++ main.js
@@ -1,39 +1,68 @@
-const body = document.getElementsByTagName("body")[0];
+const weatherContainer = document.getElementById("weatherContainer");
 const input = document.getElementById("input");
 const btn = document.getElementById("searchBtn");
 
+const url = `https://rami-weather.herokuapp.com/weather/`;
+
 const getWeather = async function (city) {
   console.log("loading data.....");
-  const weather = await $.get(
-    `https://rami-weather.herokuapp.com/weather/${city}`
-  );
-  console.log(weather);
-  return weather;
-};
-//https://rami-weather.herokuapp.com/
-btn.addEventListener("click", async function () {
-  const city = input.value;
-
   try {
-    const weather = await getWeather(city);
-    input.value = "";
-    const mainContainer = document.createElement("div");
-    mainContainer.id = "mainContainer";
-    const temp = document.createElement("div");
-    temp.id = "temp";
-    temp.innerText = `${weather.temprature}`;
-    const weatherStatusImg = document.createElement("img");
-    weatherStatusImg.id = "weatherStatusImg";
-    weatherStatusImg.src = `https://rami-weather.herokuapp.com/${weather.conditionPic}`;
-    const weatherCondition = document.createElement("div");
-    weatherCondition.id = "weatherCondition";
-    weatherCondition.innerText = `${weather.condition}`;
-    const cityName = document.createElement("div");
-    cityName.id = "cityName";
-    cityName.innerText = `${city}`;
-    mainContainer.append(temp, weatherStatusImg, weatherCondition, cityName);
-    body.append(mainContainer);
+    const weather = await $.get(url + city);
+    return weather;
   } catch (error) {
     alert("Please enter a valid country");
+    return error;
   }
+};
+
+const addToFav = function (city) {
+  localStorage.setItem("favCity", city);
+};
+
+const renderWeather = function (weather) {
+  const mainContainer = document.createElement("div");
+  mainContainer.id = "mainContainer";
+  const temp = document.createElement("div");
+  temp.id = "temp";
+  temp.innerText = `${weather.temprature}`;
+  const weatherStatusImg = document.createElement("img");
+  weatherStatusImg.id = "weatherStatusImg";
+  weatherStatusImg.src = `https://rami-weather.herokuapp.com/${weather.conditionPic}`;
+  const weatherCondition = document.createElement("div");
+  weatherCondition.id = "weatherCondition";
+  weatherCondition.innerText = `${weather.condition}`;
+  const cityName = document.createElement("div");
+  cityName.id = "cityName";
+  cityName.innerText = `${weather.name}`;
+  const favBtn = document.createElement("button");
+  favBtn.innerText = "add";
+  favBtn.addEventListener("click", function () {
+    addToFav(weather.name);
+  });
+  mainContainer.append(
+    temp,
+    weatherStatusImg,
+    weatherCondition,
+    cityName,
+    favBtn
+  );
+  weatherContainer.prepend(mainContainer);
+};
+
+const getFavCity = async function () {
+  const city = localStorage.getItem("favCity");
+  if (city) {
+    const weather = await getWeather(city);
+    renderWeather(weather)
+  }
+};
+
+
+btn.addEventListener("click", async function () {
+  const city = input.value;
+  const weather = await getWeather(city);
+  input.value = "";
+  renderWeather(weather);
 });
+
+getFavCity()
